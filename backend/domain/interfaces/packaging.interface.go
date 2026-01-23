820d87850d977b35d3dec37b6480f31d4e3ae473
Betelhem <betelhemyimamd@gmail.com>
2026-01-23 23:57:21 +0300

    add PackagingRepository interface

--- backend/domain/interfaces/packaging.interface.go
+++ backend/domain/interfaces/packaging.interface.go
@@ -12,3 +12,10 @@ type PackagingService interface {
 	RecordPackagingUsage(ctx context.Context, batchID string, usage []entities.PackagingUsage) error
 	GetPackagingStock(ctx context.Context) ([]*entities.Packaging, error)
 }
+
+type PackagingRepository interface {
+	InsertPackaging(ctx context.Context, packaging *entities.Packaging) error
+	UpdatePackaging(ctx context.Context, id string, packaging *entities.Packaging) error
+	RecordPackagingUsage(ctx context.Context, batchID string, usage []entities.PackagingUsage) error
+	FetchPackagingStock(ctx context.Context) ([]*entities.Packaging, error)
+}
